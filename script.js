8321d0b9e51271c30fe4227329419aed1407f75b
SarikGhanchi <mrsarik0707@gmail.com>
2025-04-15 12:19:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript for interactivity (e.g., form validation, animations, etc.)
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Message sent!");
+});
